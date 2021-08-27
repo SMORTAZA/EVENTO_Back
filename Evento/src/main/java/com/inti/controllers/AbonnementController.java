a5501dd8e08d0a974478c44fb2cc4741bbb8460a
SMORTAZA <shogofa.mortaza@hotmail.fr>
2021-08-27 08:09:58 +0200

    ajout methode update dans AbonnementController

--- Evento/src/main/java/com/inti/controllers/AbonnementController.java
+++ Evento/src/main/java/com/inti/controllers/AbonnementController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,15 @@ public class AbonnementController {
 	@GetMapping("/abonnement/{idAbonnement}")
 	public Abonnement findOne(@PathVariable("idAbonnement") Long id) {
 		return abonnementService.findOne(id);
+	}
+	
+	@PutMapping("/abonnement/{idAbonnement}")
+	public Abonnement updateUtilisateur(@PathVariable("idAbonnement") Long id, @RequestBody Abonnement abonnement) {
+		Abonnement currentAbonnement = abonnementService.findOne(id);
+		currentAbonnement.setNom(abonnement.getNom());
+		currentAbonnement.setTarif(abonnement.getTarif());
+		currentAbonnement.setDateDebut(abonnement.getDateDebut());
+		currentAbonnement.setDateFin(abonnement.getDateFin());
+		return abonnementService.saveAbonnement(currentAbonnement);
 	}
 }
